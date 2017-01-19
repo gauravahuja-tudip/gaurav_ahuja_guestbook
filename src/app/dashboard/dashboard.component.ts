2985b2934d702f845f92e29f06b33b75ce58e61c
gauravahuja-tudip <gaurav.ahuja@tudip.nl>
2017-01-19 21:50:44 +0530

    Designed Dashboard CSS, HTML and Component
    Designed dashboard html, css, component and sending data methods to send
    data to server api.

--- src/app/dashboard/dashboard.component.ts
+++ src/app/dashboard/dashboard.component.ts
@@ -1,14 +1,36 @@
 import {Component} from '@angular/core';
+import {ViewService} from "../../services/view.visitors.service";
 
 @Component({
-    moduleId: 'module.id',
     selector: 'dashboard',
     templateUrl: 'app/dashboard/dashboard.component.html',
-
+    styleUrls: ['app/dashboard/dashboard.component.css']
 })
 export class DashboardComponent{
+  allVisitors: any = [];
+  constructor(private viewService: ViewService) {
+    this.viewVisitors();
+  }
+  private viewVisitors() {
+    console.log("inside component...");
+    this.viewService.viewVisitor().subscribe(
+      data => DashboardComponent.viewSuccess(data),
+      error => DashboardComponent.viewFail(error)
+    );
+  }
 
+  private static viewSuccess(data) {
+    console.log(data);
+    //this.allVisitors = [];
+    //this.allVisitors = data;
+  }
 
+  private static viewFail(error) {
+    console.log(error);
+  }
+  public logout(){
+    localStorage.removeItem('token');
+  }
 }
 
 
